3452e883691af3d437827092a97248ed44dc5ae2
Mabel Yeboah <20127335@student.west-lothian.ac.uk>
2020-10-13 13:38:31 +0100

    add Hello Napier

---
+++ helo world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helo_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo World");
+            Console.WriteLine("Hello Napier");
+        }
+    }
+}
